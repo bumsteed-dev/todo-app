70808d409d8d8e1762c657e76dd013f969a083d4
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-02-25 20:43:04 -0500

    feat: add SVG icon components
    Add Astro icon components (Circle, CircleCheck, Edit, Exit, Plus, Trash)
    with a barrel export for clean imports.

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as IconCircle } from './IconCircle.astro';
+export { default as IconCircleCheck } from './IconCIrcleCheck.astro';
+export { default as IconEdit } from './IconEdit.astro';
+export { default as IconExit } from './IconExit.astro';
+export { default as IconPlus } from './IconPlus.astro';
+export { default as IconTrash } from './IconTrash.astro';
